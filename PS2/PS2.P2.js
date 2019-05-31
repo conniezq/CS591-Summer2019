ef98eb34a534e5c1eee719512bf1ef9d7ca5453a
conniezq <conniezq@users.noreply.github.com>
2019-05-31 14:24:37 -0400

    I guess we do not need to include PS1 files for this branch

--- PS2/PS2.P2.js
+++ PS2/PS2.P2.js
@@ -1,11 +1,11 @@
 function* printWord(str){
 	let array = str.split(' ');
-	for (var i = 0;i < array.length; i++){
+	for (let i = 0;i < array.length; i++){
 		console.log(array[i])
 	}
 }
 
-';lkiuyfdxzw44wconst printedWord = printWord('All I know is something like a bird within her sang');
+const printedWord = printWord('All I know is something like a bird within her sang');
 while(printedWord.next().done == false){
 	console.log();
 }
